239c43da140ead96b5d4eb07890d92dcc6d829bf
Anton Holmberg <anton.holmberg@greenely.se>
2020-09-21 20:44:00 +0200

    Make all config dynamic

--- main.js
+++ main.js
@@ -4,27 +4,24 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-const bannedWords = [
-  "covid",
-  "corona",
-  "chinese government",
-  "the virus",
-  "sars cov 2",
-].map((word) => word.toLocaleLowerCase());
+client.on("message", async (message) => {
+  const bannedWords = process.env.BANNED_WORDS.split(",").map((word) =>
+    word.toLocaleLowerCase().trim()
+  );
 
-const restrictedUsers = ["skomakare"];
-
-client.on("message", (message) => {
+  const restrictedUsers = process.env.RESTRICTED_USERS.split(",").map((user) =>
+    user.toLocaleLowerCase().trim()
+  );
   // If the author is not restricted abort.
-  if (!restrictedUsers.includes(message.author.username)) {
+  if (!restrictedUsers.includes(message.author.username.toLocaleLowerCase())) {
     return;
   }
 
   // Check if the message contains any banned words
   // if so delete it.
-  for (word in bannedWords) {
+  for (const word of bannedWords) {
     if (message.content.toLowerCase().includes(word)) {
-      message.delete();
+      await message.delete();
       break;
     }
   }
